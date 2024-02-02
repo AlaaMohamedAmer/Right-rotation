ed97aaa128ec5e6be38b0f1d365f1338969555c6
alaa <aamal79545@gmail.com>
2024-02-02 22:24:55 +0200

    first commit

---
+++ Right rotation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Right_rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4 };
+            Right right = new Right();
+            right.RigthRotate(arr);
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+    public class Right
+    {
+        public void RigthRotate(int[] arr)
+        {
+            int x = arr[arr.Length - 1];
+            for (int i = (arr.Length - 1); i > 0; i--)
+            {
+                arr[i] = arr[i-1];
+            }
+            arr[0] = x;
+        }
+    }
+}
